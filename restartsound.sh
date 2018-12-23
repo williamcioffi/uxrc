0f3ee1adb6e70fca42b0a9cc59d80aac421ceee9
williamcioffi <williamcioffi@gmail.com>
2018-12-22 19:21:09 -0500

    added instructions for isntall

--- restartsound.sh
+++ restartsound.sh
@@ -2,6 +2,7 @@
 
 # maybe this isn't the best practice to have sudo in the middle? 
 # but pulseaudio -k needs to be run by the user i think...
+# put this in /usr/local/bin probably
 
 pulseaudio -k
 sudo alsa force-reload
